33ff0d82743aea310e838fe484efe826936c0a8f
DNK <zarazablin@gmail.com>
2018-06-21 20:44:04 +0300

    com.javarush.task.task27.task2709 end

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2709/ConsumerTask.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2709/ConsumerTask.java
@@ -11,7 +11,10 @@ public class ConsumerTask implements Runnable {
 
     public void run() {
         while (!stopped) {
-            transferObject.get();
+            synchronized (transferObject) {
+                transferObject.get();
+            }
+
         }
     }
 
