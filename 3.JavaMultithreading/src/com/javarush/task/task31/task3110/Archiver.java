59aa7ed0e7f25b8e12804c9a53fb3980f437001d
DNK <zarazablin@gmail.com>
2018-06-24 14:48:54 +0300

    com.javarush.task.task31.task3110 part8 end

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,30 +1,48 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.Command;
-import com.javarush.task.task31.task3110.command.ExitCommand;
-
-import java.io.BufferedReader;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import static com.javarush.task.task31.task3110.Operation.*;
 
 public class Archiver {
 
     public static void main(String[] args) throws Exception {
+        Operation operation;
+        while (true) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+                if (operation == EXIT) break;
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        }
+    }
 
-        System.out.println("Enter full path of archive: ");
-        Path zipArchivePath = Paths.get(reader.readLine());
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(EXIT.ordinal() + " - выход");
 
-        System.out.println("Enter full path of file: ");
-        Path filePath = Paths.get(reader.readLine());
 
-        ZipFileManager zipFileManager = new ZipFileManager(zipArchivePath);
-        zipFileManager.createZip(filePath);
+        int numberOfOperation = ConsoleHelper.readInt();
 
-        Command exitCommand = new ExitCommand();
-        exitCommand.execute();
+        switch (numberOfOperation){
+            case 0: return CREATE;
+            case 1: return ADD;
+            case 2: return REMOVE;
+            case 3: return EXTRACT;
+            case 4: return CONTENT;
+            case 5: return EXIT;
+            default: return EXIT;
+        }
+
     }
 }
