d49403e7448da9678f9cfa98aa34a5e1a9bf0f71
DNK <zarazablin@gmail.com>
2018-06-30 12:21:01 +0300

    com.javarush.task.task31.task3110 part12 begin

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -9,11 +9,8 @@ import java.nio.file.Paths;
 public abstract class ZipCommand implements Command {
 
     public ZipFileManager getZipFileManager() throws Exception{
-        ConsoleHelper.writeMessage("Введите полный путь файла архива: ");
-        String enteredPath = ConsoleHelper.readString();
-        Path zipFilePath = Paths.get(enteredPath);
-        ZipFileManager zipFileManager = new ZipFileManager(zipFilePath);
-        return zipFileManager;
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
     }
-
 }
