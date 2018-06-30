857c59cfa6e8bea20665b6fc2b5b3576084d2bed
DNK <zarazablin@gmail.com>
2018-06-30 11:46:21 +0300

    com.javarush.task.task31.task3110 part10 end

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task31.task3110;
 
-import java.io.IOException;
-import java.io.InputStream;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -16,24 +19,47 @@ public class ZipFileManager {
 
     public void createZip(Path source) throws Exception {
 
-        try(InputStream inputStream = Files.newInputStream(source);
-            ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(zipFile)))
-        {
-            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
-            zos.putNextEntry(zipEntry);
-
-            int bufferSize = inputStream.available() > 64000 ? 64000 : inputStream.available();
-            byte[] buffer = new byte[bufferSize];
-            while (inputStream.available() > 0) {
-                if (inputStream.available() < bufferSize) bufferSize = inputStream.available();
-
-                inputStream.read(buffer, 0, bufferSize);
-                zos.write(buffer);
-            }
-            zos.closeEntry();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (Files.notExists(zipFile.getParent())) {
+            Files.createDirectory(zipFile.getParent());
         }
+
+        try(ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            if (Files.isRegularFile(source)) {
+                addNewZipEntry(zos, source.getParent(), source.getFileName());
+            } else if (Files.isDirectory(source)) {
+                FileManager fileManager = new FileManager(source);
+                ArrayList<Path> fileNames = new ArrayList<>(fileManager.getFileList());
+                for (Path path : fileNames) {
+                    addNewZipEntry(zos, source, path);
+                }
+            }
+            else throw new PathIsNotFoundException();
+        }
+    }
+
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
+
+        Path fullPath = filePath.resolve(fileName);
+        try (InputStream inputStream = Files.newInputStream(fullPath)) {
+            ZipEntry zipEntry = new ZipEntry(fileName.toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            copyData(inputStream, zipOutputStream);
+            zipOutputStream.closeEntry();
+        }
+    }
+
+    private void copyData(InputStream in, OutputStream out) throws Exception {
+
+        byte[] buffer = new byte[8 * 1024];
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get("D:\\DEV\\JavaRushTasks\\Test\\test.zip"));
+        zipFileManager.createZip(Paths.get("D:\\DEV\\JavaRushTasks\\Files"));
     }
 }
--- Test/Test.java
+++ Test/Test.java
@@ -1,29 +1,49 @@
 import com.sun.xml.internal.ws.api.ha.StickyFeature;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 class Test {
     public static void main(String[] args) {
-        processIncomingMessage("дима: дата");
+        Path pathOfZipFile = Paths.get("D:\\DEV\\JavaRushTasks\\Test\\test.zip");
+        Path pathPack = Paths.get("D:\\DEV\\JavaRushTasks\\Test\\equals\\wefwef.txt");
+        try (ZipOutputStream zos = new ZipOutputStream(Files.newOutputStream(pathOfZipFile));
+             InputStream is = Files.newInputStream(pathPack)) {
+            ZipEntry zipEntry = new ZipEntry(pathPack.toString());
+            zos.putNextEntry(zipEntry);
 
+            copyData(is, zos);
+
+            zos.closeEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    protected static void processIncomingMessage(String message) {
-        if (message.contains(":")) {
-            String name = message.split(":")[0];
-            String customerRequest = message.split(":")[1].trim();
-            Calendar cal = new GregorianCalendar();
+    private static void copyData(InputStream in, OutputStream out) throws Exception {
 
-            switch (customerRequest) {
-                case "дата":
-                    SimpleDateFormat format = new SimpleDateFormat("d.MM.YYYY");
-                    System.out.println(format.format(cal.getTime()));
-            }
+        int bufferSize = in.available() > 64000 ? 64000 : in.available();
+        byte[] buffer = new byte[bufferSize];
+        while (in.available() > 0) {
+            if (in.available() < bufferSize) bufferSize = in.available();
+
+            in.read(buffer, 0, bufferSize);
+            out.write(buffer);
         }
     }
 
