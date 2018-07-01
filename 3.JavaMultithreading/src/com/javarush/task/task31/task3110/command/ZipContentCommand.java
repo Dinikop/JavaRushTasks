0b3fe98d483794ba96ec590ec2278faed0db36c0
DNK <zarazablin@gmail.com>
2018-07-01 13:37:27 +0300

    com.javarush.task.task31.task3110 part15 begin

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -7,15 +7,19 @@ import com.javarush.task.task31.task3110.ZipFileManager;
 import java.util.List;
 
 public class ZipContentCommand extends ZipCommand {
-
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
         ZipFileManager zipFileManager = getZipFileManager();
+
         ConsoleHelper.writeMessage("Содержимое архива:");
-        List<FileProperties> filePropertiesList = zipFileManager.getFilesList();
-        for (FileProperties fileProperties :filePropertiesList) {
-            ConsoleHelper.writeMessage(fileProperties.toString());
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
         }
+
         ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
