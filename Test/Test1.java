bbaee1f83891bb7df4b2f4c9417b7af9daadf074
DNK <zarazablin@gmail.com>
2018-07-17 20:23:50 +0300

    com.javarush.task.task28.task2803 end

--- Test/Test1.java
+++ Test/Test1.java
@@ -1,11 +1,12 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Test1 {
     public static void main(String[] args) {
-        int numberOfLocks = 12;
+        int min = 1;
+        int max = 4;
 
-        for (int i = 0; i <= 1000; i++) {
-            System.out.printf("i: %s, i / nol: %s \n", i, i % numberOfLocks);
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(ThreadLocalRandom.current().nextInt(min, max + 1));
         }
-
-
     }
 }
