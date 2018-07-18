123e6f85cf9f26bd21ec9079335cffc0ff7e96f1
DNK <zarazablin@gmail.com>
2018-07-18 23:30:22 +0300

    com.javarush.task.task28.task2806 end

--- 3.JavaMultithreading/src/com/javarush/task/task28/task2806/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task28/task2806/Solution.java
@@ -8,9 +8,30 @@ import java.util.concurrent.TimeUnit;
 Знакомство с Executors
 */
 public class Solution {
+
+    private static int threadLocalId = 0;
+
     public static void main(String[] args) throws InterruptedException {
         //Add your code here
 
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        for (int i = 0; i < 10; i++) {
+
+            executorService.submit(new Runnable() {
+
+                @Override
+                public void run() {
+//                    System.out.println("something do");
+                    doExpensiveOperation(++threadLocalId);
+                }
+            });
+
+
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
         /* output example
 pool-1-thread-2, localId=2
 pool-1-thread-1, localId=1
