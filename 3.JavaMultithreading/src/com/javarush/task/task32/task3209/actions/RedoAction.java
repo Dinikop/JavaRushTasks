36cc82bc1454b5095b9835fd1f8949b490e190b3
DNK <zarazablin@gmail.com>
2018-08-02 21:21:47 +0300

    com.javarush.task.task28.task3209 part8 begin

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/RedoAction.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task32.task3209.actions;
+
+import com.javarush.task.task32.task3209.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class RedoAction extends AbstractAction {
+
+    public RedoAction(View view) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/UndoAction.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task32.task3209.actions;
+
+import com.javarush.task.task32.task3209.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class UndoAction extends AbstractAction {
+
+    public UndoAction(View view) {}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
