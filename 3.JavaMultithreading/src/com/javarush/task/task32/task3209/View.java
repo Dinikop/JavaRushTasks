76dd2131a562695b6717c343a97a03e9b95bd35b
DNK <zarazablin@gmail.com>
2018-08-02 20:28:45 +0300

    com.javarush.task.task28.task3209 part6 end

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,8 +1,10 @@
 package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -37,7 +39,17 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        JScrollPane scrollHTMLPane = new JScrollPane(htmlTextPane);
+        tabbedPane.addTab("HTML", scrollHTMLPane);
+        JScrollPane scrollPlainTextPane = new JScrollPane(plainTextPane);
+        tabbedPane.addTab("Текст", scrollPlainTextPane);
 
+        tabbedPane.setPreferredSize(new Dimension(500, 500));
+
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+
+        this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void initMenuBar() {
