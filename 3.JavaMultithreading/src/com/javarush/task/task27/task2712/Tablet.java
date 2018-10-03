e9316ddea9d83447773165e25da558dcd20b5d21
dinikop <zarazablin@gmail.com>
2018-10-03 19:13:44 +0300

    com.javarush.task.task27.task271 part4

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,10 +3,12 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable {
+
     final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
@@ -14,14 +16,21 @@ public class Tablet {
         this.number = number;
     }
 
-    public void createOrder() {
+    public Order createOrder() {
 
+        Order order = null;
         try {
-            Order order = new Order(this);
+            order = new Order(this);
+            System.out.println(order);
+            setChanged();
+            notifyObservers(order);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
+        return order;
     }
+
+
 
     @Override
     public String toString() {
