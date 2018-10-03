48a3a22989d317852010a3866bb1f70317dff77e
dinikop <zarazablin@gmail.com>
2018-10-03 19:22:18 +0300

    com.javarush.task.task27.task271 part5

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
@@ -3,7 +3,7 @@ package com.javarush.task.task27.task2712.kitchen;
 import java.util.Observable;
 import java.util.Observer;
 
-public class Cook implements Observer {
+public class Cook extends Observable implements Observer {
     private String name;
 
     public Cook(String name) {
@@ -13,6 +13,8 @@ public class Cook implements Observer {
     @Override
     public void update(Observable o, Object arg) {
         System.out.println("Start cooking - " + arg);
+        setChanged();
+        notifyObservers(arg);
     }
 
     @Override
