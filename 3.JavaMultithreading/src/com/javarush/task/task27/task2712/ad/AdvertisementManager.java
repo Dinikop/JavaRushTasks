a55f5feaf08df4517da9acdceef73d3bcfa5a587
dinikop <zarazablin@gmail.com>
2018-10-03 20:46:39 +0300

    com.javarush.task.task27.task271 part9

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,4 +1,14 @@
 package com.javarush.task.task27.task2712.ad;
 
 public class AdvertisementManager {
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+    private int timeSeconds;
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public void processVideos() {
+        System.out.println("calling processVideos method");
+    }
 }
