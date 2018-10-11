94ff3e179b31fc3037842d6d29cb17e68a9255f9
dinikop <zarazablin@gmail.com>
2018-10-11 22:18:29 +0300

    com.javarush.task.task27.task271 part10

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task27.task2712.ad;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     private int timeSeconds;
@@ -11,5 +15,54 @@ public class AdvertisementManager {
     public void processVideos() throws NoVideoAvailableException {
         if (storage.list().isEmpty()) throw new NoVideoAvailableException();
 
+//        List<Advertisement> sortedList = storage.list()
+//                .stream()
+//                .sorted(Comparator.comparing(Advertisement::getAmountPerOneDisplaying)
+//                        .thenComparing(Advertisement::getDuration).reversed())
+//                .collect(Collectors.toList());
+
+        long[][] cost = new long[storage.list().size()][timeSeconds];
+
+        for (int k = 1; k < storage.list().size(); k++) {  //предметы
+            for (int s = 1; s < timeSeconds; s++) {        //длительность
+                if (s >= storage.list().get(k).getDuration()) {
+                    cost[k][s] = Long.max(cost[k-1][s], cost[k-1][s-storage.list().get(k).getDuration()] + storage.list().get(k).getAmountPerOneDisplaying());
+                }
+                else cost[k][s] = cost[k-1][s];
+            }
+        }
+
+        for (int i = 0; i < cost.length; i++) {
+            for (int j = 0; j < cost[i].length; j++) {
+                System.out.print(cost[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        List<Advertisement> list = new ArrayList<>();
+        int k = storage.list().size() - 1;
+        int s = timeSeconds - 1;
+
+        while (true) {
+            if (cost[k][s] == 0) break;
+            if (cost[k-1][s] == cost[k][s]) {
+                k--;
+            }
+            else {
+                list.add(storage.list().get(k));
+                k--;
+                s -= storage.list().get(k).getDuration();
+            }
+        }
+
+        list.forEach((x) -> System.out.println(x + " " + x.getAmountPerOneDisplaying() + " " + x.getDuration()) );
+
     }
+
+    public static void main(String[] args) {
+        AdvertisementManager advertisementManager = new AdvertisementManager(2000);
+        advertisementManager.processVideos();
+    }
+
+
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -10,9 +10,12 @@ public class AdvertisementStorage {
 
     private AdvertisementStorage() {
         Object someContent = new Object();
-        new Advertisement(someContent, "First Video", 5000, 100, 3 * 60); // 3 min
-        new Advertisement(someContent, "Second Video", 100, 10, 15 * 60); //15 min
-        new Advertisement(someContent, "Third Video", 400, 2, 10 * 60); //10 min
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min)
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min)
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
+        videos.add(new Advertisement(someContent, "Fourth Video", 1000, 4, 5 * 60)); //5 min
+        videos.add(new Advertisement(someContent, "Fifth Video", 200, 30, 6 * 60)); //6 min
+        videos.add(new Advertisement(someContent, "Sixth Video", 200, 30, 7 * 60)); //7 min
     }
 
     public static AdvertisementStorage getInstance() {
